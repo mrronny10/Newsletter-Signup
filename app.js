a884a61553194a8e7329b7c3b7199966011593d3
ronak sen <ronaksen_580>
2023-04-01 13:38:58 +0530

    First Commit

--- app.js
+++ app.js
@@ -64,7 +64,12 @@ app.post("/", function(req, res){
 
 });
 
-app.listen(3000,function() {
+app.post("/failure", function(req,res){
+    res.redirect("/")
+})
+
+
+app.listen(3000, function() {
     console.log("Server is running on port 3000");
 })
 
